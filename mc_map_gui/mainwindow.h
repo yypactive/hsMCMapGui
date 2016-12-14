b7708e1bbdfc812b85063f624948f6bc2a7bf39a
yypactive <yypactive@126.com>
2016-12-14 21:55:03 +0800

    Add files via upload
    unfinished

---
+++ mc_map_gui/mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QProcess>
+#include <ctime>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_toolButton_input_clicked();
+
+    void on_toolButton_output_clicked();
+
+    void on_spinBox_thread_valueChanged(int arg1);
+
+    void on_lineEdit_input_textEdited(const QString &arg1);
+
+    void on_lineEdit_output_textEdited(const QString &arg1);
+
+    void on_buttonBox_accepted();
+
+    void map_finish();
+
+    bool check_config();
+
+    void on_lineEdit_textEdited(const QString &arg1);
+
+private:
+    Ui::MainWindow *ui;
+    QString mInputPath;
+    QString mOutputPath;
+    QString mOutputName;
+    int mThreadNum;
+    time_t mStartTime, mEndTime;
+    QProcess *mMapProcess;
+    bool mIsRunning;
+};
+
+#endif // MAINWINDOW_H
---
+++ mc_map_gui/mc_map.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-11-28T11:41:45
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = mc_map
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
